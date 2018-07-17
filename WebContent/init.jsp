900d102caf5e0e0b239dd2a7f701cbcba8c14d28
201-08 <201-08@201-08-PC>
2018-07-17 17:34:52 +0900

    first

---
+++ WebContent/init.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/custom.css">
+<title>DB init</title>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+	
+</script>
+</head>
+<body>
+	<div class="container">
+		데이터 베이스 초기화 완료		
+	</div>	
+
+	</script>
+</body>
+</html>
---
+++ src/chat/ChatServerController.java
@@ -0,0 +1,27 @@
+package chat;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ChatServerInit")
+public class ChatServerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ChatDAO chatDAO = new ChatDAO();
+		chatDAO.initDB();
+		
+		request.getRequestDispatcher("init.jsp").forward(request, response);
+	}
+	
+}
